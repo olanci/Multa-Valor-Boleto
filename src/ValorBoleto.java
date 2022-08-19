18ac7b8b068d92f6bfe33e4e2bc598109f99b592
olanci <olanci@gmail.com>
2022-08-19 16:54:30 -0300

    first commit

---
+++ src/ValorBoleto.java
@@ -0,0 +1,32 @@
+public class ValorBoleto {
+    public static void main(String[] args) throws Exception {
+        
+        
+        
+        double ValorBoleto;
+        double valorMulta;
+        double valorTotal;
+
+
+        ValorBoleto= 800 ;
+        valorMulta=ValorBoleto*0.10;
+        valorTotal=ValorBoleto+valorMulta;
+
+
+    System.out.println("Valor do seu Boleto " +ValorBoleto+" R$");
+    System.out.println("Percentual da multa: 10% ");
+    System.out.println("Valor da Multa "+valorMulta+" R$");
+    System.out.println("Total a Paga "+valorTotal+" R$");
+    
+    }
+
+}                     
+                   
+               
+               
+        
+             
+              
+                
+        
+ 
